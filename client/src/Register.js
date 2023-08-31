6b4721a3ae9ff7861e452d6417f66c79719c3a6b
imsekhar4545 <135575542+imsekhar4545@users.noreply.github.com>
2023-08-31 18:37:24 +0530

    Update Register.js

--- client/src/Register.js
+++ client/src/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
     }
     const submitHandler = e =>{
         e.preventDefault();
-        axios.post('http://localhost:5000/register',data).then(
+        axios.post('https://bookshelf-server-ve0m.onrender.com/register',data).then(
             res => {alert(res.data);setData({
                 username:'',
                 email:'',
